66351e7bccf5d3fdb83ce624aa12576da5b88a4e
DoomSlayer <mustafa.hazeen@outlook.com>
2025-07-08 06:53:06 +0300

    Implement pickup item initialization and interaction
    Added logic to initialize pickups from data tables, manage item references, and handle interaction and focus events. Updated APickup to implement the interaction interface, added new methods for item initialization, and refactored mesh and property handling. Also added new assets and data tables to support item pickups.

--- Source/ShowcaseProject/Private/World/Pickup.cpp
+++ Source/ShowcaseProject/Private/World/Pickup.cpp
@@ -3,12 +3,18 @@
 
 #include "World/Pickup.h"
 
+#include "Items/ItemBase.h"
+
 // Sets default values
 APickup::APickup()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>("Pickup Mesh");
+	PickupMesh->SetSimulatePhysics((false));
+	SetRootComponent(PickupMesh);
+	
 }
 
 // Called when the game starts or when spawned
@@ -16,12 +22,105 @@ void APickup::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	InitializePickup(UItemBase::StaticClass(), ItemQuantity);
 }
 
-// Called every frame
-void APickup::Tick(float DeltaTime)
+void APickup::InitializePickup(const TSubclassOf<UItemBase> BaseClass, const int32 InQuantity)
 {
-	Super::Tick(DeltaTime);
+	if (ItemDataTable && !DesiredItemID.IsNone())
+	{
+		const FItemData* ItemData = ItemDataTable->FindRow<FItemData>(DesiredItemID, DesiredItemID.ToString());
 
+		ItemReference = NewObject<UItemBase>(this, BaseClass);
+
+		ItemReference->ItemID = ItemData->ItemID;
+		ItemReference->ItemType = ItemData->ItemType;
+		ItemReference->ItemQuality = ItemData->ItemQuality;
+		ItemReference->ItemNumericData = ItemData->ItemNumericData;
+		ItemReference->ItemTextData = ItemData->ItemTextData;
+		ItemReference->ItemAssetData = ItemData->ItemAssetData;
+
+		InQuantity <= 0 ? ItemReference->SetQuantity(1) : ItemReference->SetQuantity(InQuantity);
+
+		PickupMesh->SetStaticMesh(ItemData->ItemAssetData.Mesh);
+
+		UpdateInteractableData();
+	}
+}
+
+void APickup::InitializeDrop(UItemBase* ItemToDrop, const int32 InQuantity)
+{
+	ItemReference = ItemToDrop;
+	InQuantity <= 0 ? ItemReference->SetQuantity(1) : ItemReference->SetQuantity(InQuantity);
+	ItemReference->ItemNumericData.Weight = ItemToDrop->GetItemSingleWeight();
+	PickupMesh->SetStaticMesh(ItemReference->ItemAssetData.Mesh);
+	
+	UpdateInteractableData();
+}
+
+void APickup::UpdateInteractableData()
+{
+	InstanceInteractableData.InteractableType = EInteractableType::Pickup;
+	InstanceInteractableData.Action = ItemReference->ItemTextData.InteractionText;
+	InstanceInteractableData.Name = ItemReference->ItemTextData.Name;
+	InstanceInteractableData.Quantity = ItemReference->Quantity;
+	InteractableData = InstanceInteractableData;
+}
+
+void APickup::BeginFocus()
+{
+	if (PickupMesh)
+	{
+		PickupMesh->SetRenderCustomDepth(true);
+	}
+}
+
+void APickup::EndFocus()
+{
+	if (PickupMesh)
+	{
+		PickupMesh->SetRenderCustomDepth(false);
+	}
+}
+
+void APickup::Interact(AShowcaseProjectCharacter* PlayerCharacter)
+{
+	if (PlayerCharacter)
+	{
+		TakePickup(PlayerCharacter);
+	}
+}
+
+void APickup::TakePickup(const AShowcaseProjectCharacter* Taker)
+{
+	if (IsPendingKillPending())
+	{
+		if (ItemReference)
+		{
+			//if (UInventoryComponent* PlayerInventory = Taker->GetInventory())
+			/**	{
+			 *	Try To add the item to the player's inventory
+			 *	Based on the return value, we can either destroy the pickup or not
+				} **/
+		}
+	}
+}
+
+void APickup::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName ChangedPropertyName = PropertyChangedEvent.Property ? PropertyChangedEvent.Property->GetFName() : NAME_None;
+
+	if (ChangedPropertyName == GET_MEMBER_NAME_CHECKED(APickup, DesiredItemID))
+	{
+		if (ItemDataTable)
+		{
+			if (const FItemData* ItemData = ItemDataTable->FindRow<FItemData>(DesiredItemID, DesiredItemID.ToString()))
+			{
+				PickupMesh->SetStaticMesh(ItemData->ItemAssetData.Mesh);
+			}
+		}
+	}
 }
 
--- Source/ShowcaseProject/Public/World/Pickup.h
+++ Source/ShowcaseProject/Public/World/Pickup.h
@@ -4,23 +4,63 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Interfaces/InteractionInterface.h"
 #include "Pickup.generated.h"
 
+class UItemBase;
+class UDataTable;
+
 UCLASS()
-class SHOWCASEPROJECT_API APickup : public AActor
+class SHOWCASEPROJECT_API APickup : public AActor, public IInteractionInterface
 {
 	GENERATED_BODY()
 	
-public:	
+public:
+	//Properties
+
+
+
+	//Functions
 	// Sets default values for this actor's properties
 	APickup();
 
+	void InitializePickup(const TSubclassOf<UItemBase> BaseClass, const int32 InQuantity);
+
+	void InitializeDrop(UItemBase* ItemToDrop, const int32 InQuantity);
+
+	FORCEINLINE UItemBase* GetItemData() const {return ItemReference;};
+
+	virtual void BeginFocus() override;
+	virtual void EndFocus() override;
+	
 protected:
+	//Properties
+	UPROPERTY(VisibleAnywhere, Category="Pickup | Components")
+	UStaticMeshComponent* PickupMesh;
+
+	UPROPERTY(EditInstanceOnly, Category="Pickup | Item Initialization")	
+	UDataTable* ItemDataTable;
+	
+	UPROPERTY(EditInstanceOnly, Category="Pickup | Item Initialization")
+	FName DesiredItemID;
+	
+	UPROPERTY(VisibleAnywhere, Category="Pickup | Item Reference")
+	UItemBase* ItemReference;
+	
+	UPROPERTY(EditInstanceOnly, Category="Pickup | Item Initialization")
+	int32 ItemQuantity;
+	
+	UPROPERTY(VisibleInstanceOnly, Category="Pickup | Interaction")
+	FInteractableData InstanceInteractableData;
+	
+	//Functions
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+	virtual void Interact(AShowcaseProjectCharacter* PlayerCharacter) override;
+	void UpdateInteractableData();
+	void TakePickup(const AShowcaseProjectCharacter* Taker);
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+	
 };
